79cc4f3e62e4742026801aa3250a92e5f48a0121
Jordan <jordanashment@gmail.com>
2019-05-18 08:27:07 -0600

    Adding Template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      My App
+      <Header />
+      <About/>
+      <Resume />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
