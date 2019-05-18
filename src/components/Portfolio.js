50503e304962ed50436364bdaf06e6248f53cac9
Jordan <jordanashment@gmail.com>
2019-05-18 09:40:35 -0600

    Adding components

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,9 +3,181 @@ import React, {Component} from 'react';
 class Portfolio extends Component {
     render() {
         return (
-            <div>
-            Portfolio
+            <section id="portfolio">
+
+      <div className="row">
+
+         <div className="twelve columns collapsed">
+
+            <h1>Check Out Some of My Works.</h1>
+
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+
+          	   <div className="columns portfolio-item">
+                  <div className="item-wrap">
+
+                     <a href="#modal-01" title="">
+                        <img alt="" src="images/portfolio/coffee.jpg" />
+                        <div className="overlay">
+                           <div className="portfolio-item-meta">
+          					      <h5>Coffee</h5>
+                              <p>Illustrration</p>
+          					   </div>
+                        </div>
+                        <div className="link-icon"><i className="icon-plus"></i></div>
+                     </a>
+
+                  </div>
+          		</div> 
+
+               <div className="columns portfolio-item">
+                  <div className="item-wrap">
+
+                     <a href="#modal-02" title="">
+                        <img alt="" src="images/portfolio/console.jpg" />
+                        <div className="overlay">
+                           <div className="portfolio-item-meta">
+          					      <h5>Console</h5>
+                              <p>Web Development</p>
+          					   </div>
+                        </div>
+                        <div className="link-icon"><i className="icon-plus"></i></div>
+                     </a>
+
+                  </div>
+          		</div> 
+
+               <div className="columns portfolio-item">
+                  <div className="item-wrap">
+
+                     <a href="#modal-03" title="">
+                        <img alt="" src="images/portfolio/judah.jpg" />
+                        <div className="overlay">
+                           <div className="portfolio-item-meta">
+          					      <h5>Judah</h5>
+                              <p>Webdesign</p>
+          					   </div>
+                        </div>
+                        <div className="link-icon"><i className="icon-plus"></i></div>
+                     </a>
+
+                  </div>
+          		</div> 
+
+               <div className="columns portfolio-item">
+                  <div className="item-wrap">
+
+                     <a href="#modal-04" title="">
+                        <img alt="" src="images/portfolio/into-the-light.jpg" />
+                        <div className="overlay">
+                           <div className="portfolio-item-meta">
+          					      <h5>Into The Light</h5>
+                              <p>Photography</p>
+          					   </div>
+                        </div>
+                        <div className="link-icon"><i className="icon-plus"></i></div>
+                     </a>
+
+                  </div>
+          		</div> 
+
+               <div className="columns portfolio-item">
+                  <div className="item-wrap">
+
+                     <a href="#modal-05" title="">
+                        <img alt="" src="images/portfolio/farmerboy.jpg" />
+                        <div className="overlay">
+                           <div className="portfolio-item-meta">
+          					      <h5>Farmer Boy</h5>
+                              <p>Branding</p>
+          					   </div>
+                        </div>
+                        <div className="link-icon"><i className="icon-plus"></i></div>
+                     </a>
+
+                  </div>
+          		</div> 
+
+               <div className="columns portfolio-item">
+                  <div className="item-wrap">
+
+                     <a href="#modal-06" title="">
+                        <img alt="" src="images/portfolio/girl.jpg" />
+                        <div className="overlay">
+                           <div className="portfolio-item-meta">
+          					      <h5>Girl</h5>
+                              <p>Photography</p>
+          					   </div>
+                        </div>
+                        <div className="link-icon"><i className="icon-plus"></i></div>
+                     </a>
+
+                  </div>
+          		</div> 
+
+               <div className="columns portfolio-item">
+                  <div className="item-wrap">
+
+                     <a href="#modal-07" title="">
+                        <img alt="" src="images/portfolio/origami.jpg" />
+                        <div className="overlay">
+                           <div className="portfolio-item-meta">
+          					      <h5>Origami</h5>
+                              <p>Illustrration</p>
+          					   </div>
+                        </div>
+                        <div className="link-icon"><i className="icon-plus"></i></div>
+                     </a>
+
+                  </div>
+          		</div> 
+
+               <div className="columns portfolio-item">
+                  <div className="item-wrap">
+
+                     <a href="#modal-08" title="">
+                        <img alt="" src="images/portfolio/retrocam.jpg" />
+                        <div className="overlay">
+                           <div className="portfolio-item-meta">
+          					      <h5>Retrocam</h5>
+                              <p>Web Development</p>
+          					   </div>
+                        </div>
+                        <div className="link-icon"><i className="icon-plus"></i></div>
+                     </a>
+
+                  </div>
+          		</div>  
+
+            </div> 
+
+         </div> 
+
+
+
+
+
+
+		      <img className="scale-with-grid" src="images/portfolio/modals/m-retrocam.jpg" alt="" />
+
+		      <div className="description-box">
+			      <h4>Retrocam</h4>
+			      <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+               <span className="categories"><i className="fa fa-tag"></i>Webdesign, Photography</span>
+		      </div>
+
+            <div className="link-box">
+               <a href="http://www.behance.net">Details</a>
+		         <a className="popup-modal-dismiss">Close</a>
             </div>
+
+	      </div>
+
+
+      
+
+   </section>
         );
     }
 }
