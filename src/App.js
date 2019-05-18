add12a5a52f81983189b1d61c490dd5ab7b880bf
Jordan <jordanashment@gmail.com>
2019-05-18 11:31:09 -0600

    Creating json data and setting up ajax request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import $ from 'jquery';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
@@ -7,19 +8,50 @@ import Portfolio from './components/Portfolio';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { tsConstructorType } from '@babel/types';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <About/>
-      <Resume />
-      <Portfolio />
-      <Testimonials />
-      <Contact />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    }
+  }
+
+  getResumeData(){
+    $.ajax({
+      url: 'http://localhost:3000/resumeData.json',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({resumeData: data})
+      }.bind(this),
+      error: function(xhr, status, error) {
+        console.log(error);
+        alert(error)
+      }
+    })
+  }
+
+componentDidMount() {
+  this.getResumeData()
+}
+
+  render() {
+    console.log(this.state.resumeData)
+    return (
+      <div className="App">
+        <Header />
+        <About/>
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
